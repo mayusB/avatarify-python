59d94de7ab52bd356d867bf580b789f439b91c31
mayusB <54024278+mayusB@users.noreply.github.com>
2023-05-26 06:50:53 +0100

    Update install_windows.bat
    mamba package manager suggestion, as conda is slow.

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -9,12 +9,13 @@ call scripts/settings_windows.bat
 call conda create -y -n %CONDA_ENV_NAME% python=3.7
 call conda activate %CONDA_ENV_NAME%
 
-call conda install -y numpy==1.19.0 scikit-image python-blosc==1.7.0 -c conda-forge
-call conda install -y pytorch==1.7.1 torchvision cudatoolkit=11.0 -c pytorch
-call conda install -y -c anaconda git
+call mamba install -y numpy==1.19.0 scikit-image python-blosc==1.7.0
+call mamba install -y pytorch==1.7.1 -c pytorch
+call mamba install -y torchvision -c pytorch
+call mamba install -y -c anaconda git
 
 REM ###FOMM###
 call rmdir fomm /s /q
 call git clone https://github.com/alievk/first-order-model.git fomm
 
-call pip install -r requirements.txt --use-feature=2020-resolver
+call pip install -r requirements.txt
